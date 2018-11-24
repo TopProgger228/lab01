3d667ac6c65f3c3e5c249e039df64d30d292003b
TopProgger228 <dr.pain1999@gmail.com>
2018-11-25 00:57:01 +0200

    trying to start simple version of analyzer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,10 @@ import org.apache.commons.lang3.reflect.MethodUtils;
 import org.reflections.Reflections;
 import sorters.AbstractSorter;
 import fillers.*;
+import sorters.BubbleSorter;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
@@ -12,21 +14,38 @@ import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws InstantiationException, IllegalAccessException,
-            InvocationTargetException {
+            InvocationTargetException, NoSuchMethodException {
         Reflections reflections = new Reflections("sorters");
         Set<Class<? extends AbstractSorter>> set = reflections.getSubTypesOf(AbstractSorter.class);
 
-        for (Class<?> c : set){
+        for (Class<? extends AbstractSorter> c : set){
             System.out.println(c.getName());
         }
+
+        set.remove(BubbleSorter.class);
 
         Method[] methods = MethodUtils.getMethodsWithAnnotation(Fillers.class, FillerAnnotation.class);
 
         for (Method method : methods){
             System.out.println(method.getName());
-            Object obarray = method.invoke(null, 10);
+//            Object obarray = method.invoke(null, 10);
+            Object obarray = MethodUtils.invokeStaticMethod(Fillers.class, method.getName(), 10);
             int[] array = (int[]) obarray;
             System.out.println(Arrays.toString(array));
+        }
+
+        for (Class<? extends AbstractSorter> c : set){
+            System.out.println(c.getName());
+            Constructor<? extends AbstractSorter> constructor = c.getConstructor();
+            AbstractSorter object = constructor.newInstance();
+
+            for (Method m : methods){
+                System.out.println(m.getName());
+                Object obarray = MethodUtils.invokeStaticMethod(Fillers.class, m.getName(), 10);
+                int[] array = (int[]) obarray;
+                object.sort(array);
+                System.out.println(Arrays.toString(array));
+            }
         }
     }
 }

eee32b0ad8e48357a833d7f7e10a270385bf390e
TopProgger228 <dr.pain1999@gmail.com>
2018-11-24 19:28:18 +0200

    trying reflection

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,11 @@
+import org.apache.commons.lang3.reflect.MethodUtils;
 import org.reflections.Reflections;
 import sorters.AbstractSorter;
 import fillers.*;
+
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Set;
 
 public class Main {
@@ -9,14 +13,14 @@ public class Main {
         Reflections reflections = new Reflections("sorters");
         Set<Class<? extends AbstractSorter>> set = reflections.getSubTypesOf(AbstractSorter.class);
 
-        Class<?> fillers = Fillers.class;
-        Method[] methods = fillers.getDeclaredMethods();
         for (Class<?> c : set){
             System.out.println(c.getName());
         }
 
-        for (Method m : methods){
-            System.out.println(m.getName());
+        Method[] methods = MethodUtils.getMethodsWithAnnotation(Fillers.class, FillerAnnotation.class);
+
+        for (Method method : methods){
+            System.out.println(method.getName());
         }
     }
 }
